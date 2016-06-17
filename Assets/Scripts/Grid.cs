c3c1e5939e6ba7bb4dde8a1ba7210965365c2b97
Dantai Limlawan <dantai39@gmail.com>
2016-06-17 14:49:41 +0700

    add grid script

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+    public GameObject tirePrefabs;
+    public int numberOfTire = 24;
+    public float distanceBetweenTire = 2.0f;
+    public int tilePerRow = 4;
+	// Use this for initialization
+	void Start () {
+        createTire();
+	}
+	
+    void createTire()
+    {
+
+    }
+}
