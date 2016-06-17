67ced9fc085d394c082e319a6cc1112fde6fea52
Dantai Limlawan <dantai39@gmail.com>
2016-06-17 15:12:00 +0700

    update grid script

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -2,17 +2,28 @@
 using System.Collections;
 
 public class Grid : MonoBehaviour {
-    public GameObject tirePrefabs;
-    public int numberOfTire = 24;
-    public float distanceBetweenTire = 2.0f;
+    public GameObject tilePrefabs;
+    public int numberOfTile = 24;
+    public float distanceBetweenTile = 2.0f;
     public int tilePerRow = 4;
 	// Use this for initialization
 	void Start () {
-        createTire();
+        createTile();
 	}
 	
-    void createTire()
+    void createTile()
     {
-
+        float xOffset = 0;
+        float zOffset = 0;
+        for(int createdTile = 0; createdTile < numberOfTile; createdTile++)
+        {
+            xOffset += distanceBetweenTile;
+            if(createdTile % tilePerRow == 0)
+            {
+                zOffset += distanceBetweenTile;
+                xOffset = 0;
+            }
+            Instantiate(tilePrefabs, new Vector3(transform.position.x + xOffset, transform.position.y, transform.position.z + zOffset), transform.rotation);
+        }
     }
 }
